a56de5d0579f5a1169436daa5a26d4fe8ebe03bd
Felipe T C <ftorocastilla@gmail.com>
2024-07-27 15:28:15 -0300

    feat: resolve exercise 1.3

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -4,9 +4,9 @@ function Content(props) {
     console.log(props)
   return (
     <div>
-        <Part part = {props.part1} exercise = {props.exercise1}/>
-        <Part part = {props.part2} exercise = {props.exercise2}/>
-        <Part part = {props.part3} exercise = {props.exercise3}/>
+        <Part part = {props.part1} exercise = {props.exercises1}/>
+        <Part part = {props.part2} exercise = {props.exercises2}/>
+        <Part part = {props.part3} exercise = {props.exercises3}/>
     </div>
 )
 }

27503ebead265b70b1d2bf969577a26f6ece6f62
Felipe Toro <ftorocastilla@gmail.com>
2025-01-21 18:02:43 -0300

    add: exercise 2.19, add button to show aditional information when the button is clicked in the list of countries

--- part2/data-countries/src/components/ListOfCountries.jsx
+++ part2/data-countries/src/components/ListOfCountries.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import DetailOfCountry from "./DetailOfCountry";
 
 const ListOfCountries = ({ countries, search }) => {
+    const [selectedCountry, setSelectedCountry] = useState(null);
 
     const findMatches = () => {
         if (search.length === 0) return []
+
         return countries.filter((country) =>
             country.name.common.toLowerCase().includes(search)
         );
@@ -11,29 +14,59 @@ const ListOfCountries = ({ countries, search }) => {
 
     const matches = findMatches()
 
+    const showInformation = (country) => {
+        setSelectedCountry(country);
+    };
+
     return (
         <div>
             {/* Renderización condicional */}
             {matches.length === 0 && <h2>Look for a country</h2>}
             {matches.length > 10 && <h2>Too many matches, specify another filter</h2>}
-            {matches.length > 0 && matches.length <= 10 && (
+            {matches.length > 1 && matches.length <= 10 && (
                 <ul>
                     {matches.map((country) => (
-                        <li key={country.name.common}>{country.name.common}</li>
+                        <li key={country.name.common}>
+                            {country.name.common}
+                            <button onClick={() => showInformation(country)}>show</button>
+                        </li>
                     ))}
                 </ul>
             )}
             {matches.length === 1 && (
                 <div>
                     <h2>{matches[0].name.common}</h2>
-                    <p><strong>Capital: </strong> {matches[0].capital}</p>
-                    <p><strong>Area: </strong> {matches[0].area}</p>
+                    <p>
+                        <strong>Capital:</strong> {matches[0].capital}
+                    </p>
+                    <p>
+                        <strong>Area:</strong> {matches[0].area}
+                    </p>
                     <h2>Languages</h2>
                     <ul>
-                        {Object.entries(matches[0].languages).map(entry => <li key={entry[0]}> {entry[1]} </li>
-                        )}
+                        {Object.entries(matches[0].languages).map(([key, value]) => (
+                            <li key={key}>{value}</li>
+                        ))}
                     </ul>
-                    <img src={matches[0].flags.svg} alt="Bandera do pais" />
+                    <img src={matches[0].flags.svg} alt="Country flag" />
+                </div>
+            )}
+            {selectedCountry && matches.length > 1 && (
+                <div>
+                    <h2>{selectedCountry.name.common}</h2>
+                    <p>
+                        <strong>Capital:</strong> {selectedCountry.capital}
+                    </p>
+                    <p>
+                        <strong>Area:</strong> {selectedCountry.area}
+                    </p>
+                    <h2>Languages</h2>
+                    <ul>
+                        {Object.entries(selectedCountry.languages).map(([key, value]) => (
+                            <li key={key}>{value}</li>
+                        ))}
+                    </ul>
+                    <img src={selectedCountry.flags.svg} alt="Country flag" />
                 </div>
             )}
         </div>
